2c1a2ab8642254017a49cb9d59b4215b5cd2769e
Burbanit0 <72401931+Burbanit0@users.noreply.github.com>
2021-10-20 16:14:52 +0200

    new commit

--- imports/ui/TaskForm.jsx
+++ imports/ui/TaskForm.jsx
@@ -1,20 +1,17 @@
 import React, { useState } from 'react';
-import { TasksCollection } from '/imports/api/TasksCollection';
+import { TasksCollection } from '/imports/db/TasksCollection';
  
 export const TaskForm = () => {
-  const [text, setText] = useState("");
-  
+  const [text, setText] = useState('');
+
   const handleSubmit = e => {
     e.preventDefault();
 
     if (!text) return;
 
-    TasksCollection.insert({
-      text: text.trim(),
-      createdAt: new Date()
-    });
+    Meteor.call('tasks.insert', text);
 
-    setText("");
+    setText('');
   };
  
   return (
