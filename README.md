9b531aede502c8655d8e9ff40631eb1b16853a4f
BURBAN <burbanmatthieu@yahoo.fr>
2021-10-20 12:17:35 +0200

    hot reload test app

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Projet-CSW
 Projet Client Server Web en cours d'application N-tiers
 
+L'objectif est de réaliser une application 
 ``` meteor run ```
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,19 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { TasksCollection } from '/imports/api/TasksCollection';
 import { TaskForm } from './TaskForm.jsx';
 import { Task } from './Task.jsx';
 
+const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked
+    }
+  })
+};
+
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
-  return(
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <TaskForm/>
-    <ul>
-      { tasks.map(task => <Task key={ task._id } task={ task }/>) }
-    </ul>
-  </div>
+  const [hideCompleted, setHideCompleted] = useState(false);
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+
+  const tasks = useTracker(() =>
+    TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+      sort: { createdAt: -1 },
+    }).fetch()
+  );
+
+  const pendingTasksCount = useTracker(() =>
+    TasksCollection.find(hideCompletedFilter).count()
+  );
+
+  const pendingTasksTitle = `${
+    pendingTasksCount ? ` (${pendingTasksCount})` : ''
+  }`;
+
+  return (
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+            <h1>
+              📝️ To Do List
+              {pendingTasksTitle}
+            </h1>
+          </div>
+        </div>
+      </header>
+
+      <div className="main">
+        <TaskForm />
+        <div className="filter">
+         <button onClick={() => setHideCompleted(!hideCompleted)}>
+           {hideCompleted ? 'Show All' : 'Hide Completed'}
+         </button>
+       </div>
+
+        <ul className="tasks">
+          {tasks.map(task => (
+            <Task
+              key={task._id}
+              task={task}
+              onCheckboxClick={toggleChecked}
+              onDeleteClick={deleteTask}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 };
